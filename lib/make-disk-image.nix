d0c3818057fff862a18f9a1e71ff5b1dbc3a1ecc
Volodia P.-G. <volodia.parol-guarino@proton.me>
2024-06-01 08:43:18 +0200

    feat: use rsync

--- lib/make-disk-image.nix
+++ lib/make-disk-image.nix
@@ -109,6 +109,7 @@
     # We copy files with cp because `nix copy` seems to have a large memory leak
     mkdir -p ${systemToInstall.config.disko.rootMountPoint}/nix/store
     xargs cp --recursive --target ${systemToInstall.config.disko.rootMountPoint}/nix/store < ${closureInfo}/store-paths
+    ${pkgs.rsync}/bin/rsync -avhW --no-compress ${closureInfo}/store-paths/ ${systemToInstall.config.disko.rootMountPoint}/nix/store
 
     ${systemToInstall.config.system.build.nixos-install}/bin/nixos-install --root ${systemToInstall.config.disko.rootMountPoint} --system ${systemToInstall.config.system.build.toplevel} --keep-going --no-channel-copy -v --no-root-password --option binary-caches ""
     umount -Rv ${systemToInstall.config.disko.rootMountPoint}
